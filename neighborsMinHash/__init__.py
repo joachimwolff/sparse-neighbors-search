bdf46ddc114d56210b70220c2f728587964f1ab0
Joachim Wolff <wolffj@tfpool03.informatik.uni-freiburg.de>
2015-08-07 22:09:32 +0200

    added __init.py__

---
+++ neighborsMinHash/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2015 Joachim Wolff
+# Master Project
+# Tutors: Milad Miladi, Fabrizio Costa
+# Summer semester 2015
+#
+# Chair of Bioinformatics
+# Department of Computer Science
+# Faculty of Engineering
+# Albert-Ludwig-University Freiburg im Breisgau
+
+__author__ = 'joachimwolff'
+
+from neighbors.minHashKNeighborsClassifier import MinHashKNeighborsClassifier
+from neighbors.minHashNearestNeighbors import MinHashNearestNeighbors
+from computation.inverseIndex import InverseIndex
