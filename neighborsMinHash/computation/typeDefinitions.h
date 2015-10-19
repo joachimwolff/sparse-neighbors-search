55710efa7e819382f166db1f8261fcf73d78a7bf
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-10-19 18:00:22 +0200

    created header with only typedefinitions to avoid conflicts

---
+++ neighborsMinHash/computation/typeDefinitions.h
@@ -0,0 +1,34 @@
+#include <boost/numeric/mtl/mtl.hpp>
+#define MAX_VALUE 2147483647
+
+typedef std::vector<size_t> vsize_t;
+typedef std::vector<int> vint;
+typedef std::unordered_map< size_t, vsize_t > umapVector;
+typedef std::vector<vsize_t > vvsize_t;
+typedef std::vector<vint > vvint;
+typedef std::vector< std::vector<float> > vvfloat;
+typedef std::vector<std::map<size_t, size_t> > vmSize_tSize_t;
+
+typedef mtl::compressed2D<float, mtl::mat::parameters<mtl::tag::col_major> > csrMatrix;
+
+struct csrNeighborhood {
+  vvsize_t instances;
+  vvfloat features;
+  vvfloat data;
+};
+struct rawData {
+  csrMatrix* matrixData;
+  umapVector* inverseIndexData;
+};
+
+struct uniqueElement {
+  vsize_t instances;
+  vsize_t features;
+};
+
+struct neighborhood {
+  vvint* neighbors;
+  vvfloat* distances;
+};
+
+typedef std::unordered_map<size_t, uniqueElement > umap_uniqueElement;
