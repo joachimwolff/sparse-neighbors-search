b3467eb0e274c3b1290153be05bc2adea83ff707
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-02 16:11:53 +0100

    First skeleton of the bloomier filter

---
+++ neighborsMinHash/computation/bloomierFilter.cpp
@@ -0,0 +1,27 @@
+#include "bloomierFilter.h"
+
+BloomierFilter::BloomierFilter(size_t pHashSeed, size_t pM, size_t pK, size_t pQ){
+	mHashSeed = pHashSeed;
+	mM = pM;;
+	mK = pK;
+	mQ = pQ;
+}
+
+BloomierFilter::~BloomierFilter(){
+
+};
+
+vint* BloomierFilter::lookup(size_t pKey) {
+
+}
+
+void BloomierFilter::setValue(size_t pKey, size_t pValue) {
+
+}
+vint* BloomierFilter::findMatch(size_t pHashSeed, vint* pSubset) {
+
+}
+
+void BloomierFilter::create(std::map<size_t, size_t> pAssignment) {
+	
+}
--- neighborsMinHash/computation/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter.h
@@ -1,1 +1,25 @@
-class BloomierFilter {};
+#include "typeDefinitions.h"
+#ifndef BLOOMIER_FILTER_H
+#define BLOOMIER_FILTER_H
+class BloomierFilter {
+  private:
+    vint mTable1;
+    vint mTable2;
+    size_t mHashSeed;
+    size_t mM;
+    size_t mK;
+    size_t mQ;
+  public:      
+    BloomierFilter(size_t pHashSeed, size_t pM, size_t pK, size_t pQ) {
+
+    };
+    ~BloomierFilter() {
+
+    };
+    vint* lookup(size_t pKey);
+    void setValue(size_t pKey, size_t pValue);
+    vint* findMatch(size_t pHashSeed, vint* pSubset);
+    void create(std::map<size_t, size_t> pAssignment);
+
+};
+#endif // BLOOMIER_FILTER_H
