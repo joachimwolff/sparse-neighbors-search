964f1396b1b41bb0bfba63b33bddc3907f07bb62
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-04 17:56:43 +0100

    inserted conversion from distance to similarity

--- neighborsMinHash/clustering/minHashSpectralClustering.py
+++ neighborsMinHash/clustering/minHashSpectralClustering.py
@@ -11,6 +11,7 @@
 from ..neighbors import MinHash
 
 from sklearn.cluster import SpectralClustering
+import numpy as np
 
 class MinHashSpectralClustering():
     def __init__(self, n_clusters=8, eigen_solver=None, 
@@ -67,8 +68,15 @@ class MinHashSpectralClustering():
         excess_factor = self.excess_factor,
         number_of_cores = self.number_of_cores,
         chunk_size = self.chunk_size)
+        print "71"
         minHashNeighbors.fit(X, y)
+        print "73"
         graph_result = minHashNeighbors.kneighbors_graph()
+        print "75"
+        graph_result.data = np.array(np.exp(np.divide(np.multiply(np.power(graph_result.data, 2), -1), 2*1**2)))
+        print "77"
+        # X = np.exp(graph_result.multiply(graph_result).multiply(1/(2*1**2)).multiply(-1))
+        # np.exp(- graph_result ** 2 / (2. * 1 ** 2))
         self._spectralClustering.fit(graph_result)
         self.labels_ = self._spectralClustering.labels_
     def fit_predict(self, X, y=None):
