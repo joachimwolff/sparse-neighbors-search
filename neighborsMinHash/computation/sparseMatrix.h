aee45f293d8f8eda9820d03a5463d9fb2a7fbfae
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-09 14:09:34 +0100

    Added comments

--- neighborsMinHash/computation/sparseMatrix.h
+++ neighborsMinHash/computation/sparseMatrix.h
@@ -75,17 +75,32 @@ class SparseMatrixFloat {
         std::vector<sortMapFloat>* returnValue = new std::vector<sortMapFloat>(pRowIdVector.size());
         size_t pointerToMatrixElement = 0;
         size_t pointerToVectorElement = 0;
+        // iterate over all candidates
         for (size_t i = 0; i < pRowIdVector.size(); ++i) {
             sortMapFloat element; 
             element.key = pRowIdVector[i];
             element.val = 0.0;
+            // features ids are stored in mSparseMatrix. 
+            // every instances starts at index indexId*mMaxNnz --> every instance can store maximal mMaxNnz feature ids
+            // how many elements per index are stored is stored in mSizesOfInstances[indexID]
+            // iterate until both instances have no more feature ids
             while (pointerToMatrixElement < mSizesOfInstances[pRowIdVector[i]] || pointerToVectorElement < mSizesOfInstances[pRowId]) {
+                // are the feature ids of the two instances the same?
                 if (mSparseMatrix[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement] == mSparseMatrix[pRowId*mMaxNnz + pointerToVectorElement]) {
+                    // if (mSparseMatrixValues[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement] > 1 || mSparseMatrixValues[pRowId*mMaxNnz + pointerToVectorElement] > 1) {
+                    //     std::cout << "featureID: " << mSparseMatrix[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement];
+                    //     std::cout << "value: " << mSparseMatrixValues[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement];
+                    //     std::cout << "\nfeatureId_V: " << mSparseMatrix[pRowId*mMaxNnz + pointerToVectorElement];
+                    //     std::cout << "featureID_V " << mSparseMatrixValues[pRowId*mMaxNnz + pointerToVectorElement] << std::endl;
+                    // }
+                    // if they are the same substract the values, compute the square and sum it up
                     element.val += 
                     pow(mSparseMatrixValues[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement] - mSparseMatrixValues[pRowId*mMaxNnz + pointerToVectorElement], 2);
+                    // increase both counters to the next element 
                     ++pointerToMatrixElement;
                     ++pointerToVectorElement;
                 } else if (mSparseMatrix[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement] < mSparseMatrix[pRowId*mMaxNnz + pointerToVectorElement]) {
+                    // if the feature ids are unequal square only the smaller one and add it to the sum
                     element.val += pow(mSparseMatrixValues[pRowIdVector[i]*mMaxNnz + pointerToMatrixElement], 2);
                     ++pointerToMatrixElement;
                 } else {
@@ -96,6 +111,7 @@ class SparseMatrixFloat {
 
             pointerToMatrixElement = 0;
             pointerToVectorElement = 0;
+            // square root of the sum
             element.val = sqrt(element.val);
             
             (*returnValue)[i] = element;
@@ -105,6 +121,7 @@ class SparseMatrixFloat {
         if (pNneighbors > returnValue->size()) {
             numberOfElementsToSort = returnValue->size();
         }
+        // sort the values by increasing order
         std::partial_sort(returnValue->begin(), returnValue->begin()+numberOfElementsToSort, returnValue->end(), mapSortAscByValueFloat);
         return returnValue;
     };
