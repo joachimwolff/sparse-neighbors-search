a5605036404bcd49a96145babfdab904437ab47f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-17 15:28:08 +0100

    Created class

---
+++ neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
@@ -0,0 +1,9 @@
+#ifndef ORDER_AND_MATCH_FINDER_H
+#define ORDER_AND_MATCH_FINDER_H
+class OrderAndMatchFinder {
+  public:
+  	OrderAndMatchFinder(size_t pHashSeed, std::map<size_t, size_t> pKeyDict, size_t pM, size_t pK, size_t pQ){
+
+  	};
+};
+#endif // ORDER_AND_MATCH_FINDER_H
