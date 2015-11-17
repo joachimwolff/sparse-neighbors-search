a73d045e7531a9e00fa78c9a943cae8f9b06daca
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-17 15:38:09 +0100

    Corrected some datatyps

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
@@ -5,21 +5,24 @@
 #define BLOOMIER_FILTER_H
 class BloomierFilter {
   private:
-    vsize_t mTable1;
-    vsize_t mTable2;
+    vvsize_t* mTable;
+    vsize_t* mValueTable;
     size_t mHashSeed;
     size_t mM;
     size_t mK;
     size_t mQ;
     BloomierHash mBloomierHash;
     size_t mByteSize;
+    OrderAndMatchFinder mOrderAndMatchFinder;
+    std::map<size_t, size_t> mKeyDict;
   public:      
-    BloomierFilter(size_t pHashSeed, size_t pM, size_t pK, size_t pQ) {
+    BloomierFilter(size_t pHashSeed, std::map<size_t, size_t> pKeyDict, size_t pM, size_t pK, size_t pQ) {
 
     };
     ~BloomierFilter() {
 
     };
+    size_t getByteSize(size_t pQ);
     vsize_t* getTable();
     void setTable(vsize_t pTable);
     size_t getValueTable();
