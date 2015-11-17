faa568b81366704ccfd742e2d6196815eb076ec7
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-17 11:58:45 +0100

    Basic definitions of functions

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.cpp
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.cpp
@@ -13,17 +13,38 @@ BloomierFilter::~BloomierFilter(){
 
 };
 
-vint* BloomierFilter::lookup(size_t pKey) {
+vsize_t* BloomierFilter::getTable() {
 
 }
 
-void BloomierFilter::setValue(size_t pKey, size_t pValue) {
-
-}
-vint* BloomierFilter::findMatch(size_t pHashSeed, vint* pSubset) {
+void BloomierFilter::setTable(vsize_t pTable) {
 
 }
 
-void BloomierFilter::create(std::map<size_t, size_t> pAssignment) {
-	
+vsize_t* BloomierFilter::getValueTable() {
+
 }
+void BloomierFilter::setValueTable(vsize_t* pTable) {
+
+} 
+
+size_t BloomierFilter::xorOperation(size_t pValue, size_t pM, vsize_t pNeighbors) {
+
+}
+vsize_t BloomierFilter::get(size_t pKey) {
+
+}
+void BloomierFilter::set(size_t pKey, size_t pValue) {
+
+}
+
+void BloomierFilter::create(std::map<size_t, size_t> pAssignment, OrderAndMatchFinder pPiTau) {
+
+}
+
+std::string BloomierFilter::tableToString() {
+
+}
+std::pair<vsize_t, vsize_t > stringToTable(std::string pString) {
+
+} 
