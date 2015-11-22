62c997d4a2ff42a00e1c866817ecff0121de868a
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-22 20:19:47 +0100

    Added defintions of findMatch, getPiVector, getTauVector and changed return type of find

--- neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
+++ neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
@@ -1,9 +1,19 @@
+#include "../typeDefinitions.h"
 #ifndef ORDER_AND_MATCH_FINDER_H
 #define ORDER_AND_MATCH_FINDER_H
 class OrderAndMatchFinder {
+  private:
+    size_t mM;
+    size_t mK;
+    size_t mQ
+    vsize_t* mPiVector;
+    vsize_t* mTauVector;
+    bool findMatch(vsize_t* pSubset);
   public:
-  	OrderAndMatchFinder(size_t pHashSeed, std::map<size_t, size_t> pKeyDict, size_t pM, size_t pK, size_t pQ){
-
-  	};
+  	OrderAndMatchFinder(size_t pM, size_t pK, size_t pQ);
+    void find(vsize_t* pSubset);
+    vsize_t* getPiVector();
+    vsize_t* getTauVector();
+    size_t tweak (size_t pKey, vsize_t pSubset);
 };
 #endif // ORDER_AND_MATCH_FINDER_H
