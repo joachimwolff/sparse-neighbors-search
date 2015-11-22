d7b8e2bbc5a6ef0aa52310aac83563851898fb20
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-22 21:06:08 +0100

    Changed datatype of mTable, and removed a parameter from function create's definition

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
@@ -1,30 +1,29 @@
 #include "../typeDefinitions.h"
 #include "bloomierHash.h"
-#include "orderAndMatchFinder.h"
-#include "encoder.h"
+// #include "bloomierHash.h"
+// #include "orderAndMatchFinder.h"
+// #include "encoder.h"
 #ifndef BLOOMIER_FILTER_H
 #define BLOOMIER_FILTER_H
 class BloomierFilter {
   private:
-    vvsize_t* mTable;
+    bloomierTable* mTable;
     vsize_t* mValueTable;
-    size_t mHashSeed;
     size_t mM;
     size_t mK;
     size_t mQ;
     BloomierHash mBloomierHash;
-    size_t mByteSize;
+    // BloomierHash mBloomierHash;
+    // size_t mByteSize;
     OrderAndMatchFinder mOrderAndMatchFinder;
-    std::map<size_t, size_t> mKeyDict;
+    // std::map<size_t, size_t> mKeyDict;
     Encoder mEncoder;
 
   public:      
-    BloomierFilter(size_t pHashSeed, std::map<size_t, size_t> pKeyDict, size_t pM, size_t pK, size_t pQ) {
-
-    };
-    ~BloomierFilter() {
-
-    };
+    BloomierFilter(size_t pM, size_t pK, size_t pQ);
+    
+    ~BloomierFilter();
+    
     size_t getByteSize(size_t pQ);
     vsize_t* getTable();
     void setTable(vsize_t pTable);
@@ -34,7 +33,7 @@ class BloomierFilter {
     size_t xorOperation(vsize_t* pValue, vsize_t* pM, vsize_t* pNeighbors);
     vsize_t get(size_t pKey);
     bool set(size_t pKey, size_t pValue);
-    void create(std::map<size_t, size_t> pAssignment, OrderAndMatchFinder pPiTau);
+    void create(std::map<size_t, size_t> pAssignment);
     std::string tableToString();
     std::pair<vsize_t, vsize_t> stringToTable(std::string pString);
 };
