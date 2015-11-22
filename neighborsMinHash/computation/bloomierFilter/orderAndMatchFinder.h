773499dc0f6e26563e8f530e7ab5f8f77a89c932
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-22 21:02:29 +0100

    Added defintions of function computeNonSingeltns, tweak, destructor and member variables mHashesSeen, mNonSingeltons and mBloomierHash

--- neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
+++ neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
@@ -1,4 +1,7 @@
 #include "../typeDefinitions.h"
+#include <set>
+#include "bloomierHash.py"
+
 #ifndef ORDER_AND_MATCH_FINDER_H
 #define ORDER_AND_MATCH_FINDER_H
 class OrderAndMatchFinder {
@@ -8,12 +11,17 @@ class OrderAndMatchFinder {
     size_t mQ
     vsize_t* mPiVector;
     vsize_t* mTauVector;
+    std::set<size_t> mHashesSeen;
+    std::set<size_t> mNonSingeltons;
+    BloomierHash* mBloomierHash;
     bool findMatch(vsize_t* pSubset);
+    void computeNonSingeltons(std::map<size_t, size_t> pKeyValue);
+    int tweak(size_t pKey, vsize_t pSubset);
   public:
-  	OrderAndMatchFinder(size_t pM, size_t pK, size_t pQ);
+  	OrderAndMatchFinder(size_t pM, size_t pK, size_t pQ, BloomierHash* pBloomierHash);
+    ~OrderAndMatchFinder();
     void find(vsize_t* pSubset);
     vsize_t* getPiVector();
     vsize_t* getTauVector();
-    size_t tweak (size_t pKey, vsize_t pSubset);
 };
 #endif // ORDER_AND_MATCH_FINDER_H
