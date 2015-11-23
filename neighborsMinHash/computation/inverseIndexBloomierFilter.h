bd723ac8b780947eb49ebdd630a0789848a40868
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 16:46:23 +0100

    Added declaration of functions

--- neighborsMinHash/computation/inverseIndexBloomierFilter.h
+++ neighborsMinHash/computation/inverseIndexBloomierFilter.h
@@ -1,4 +1,6 @@
 #include "inverseIndex.h"
+#include "bloomierFilter/bloomierFilter.h" 
+
 #ifndef INVERSE_INDEX_BLOOMIER_FILTER_H
 #define INVERSE_INDEX_BLOOMIER_FILTER_H
 class InverseIndexBloomierFilter : public InverseIndex {
@@ -9,5 +11,9 @@ class InverseIndexBloomierFilter : public InverseIndex {
                     size_t pNumberOfCores, size_t pChunkSize,
                     size_t pMaxBinSize, size_t pMinimalBlocksInCommon,
                     size_t pExcessFactor, size_t pMaximalNumberOfHashCollisions);
+		vsize_t* computeSignature(const SparseMatrixFloat* pRawData, const size_t pInstance);
+  		umap_uniqueElement* computeSignatureMap(const SparseMatrixFloat* pRawData);
+  		void fit(const SparseMatrixFloat* pRawData);
+  		neighborhood* kneighbors(const umap_uniqueElement* signaturesMap, const int pNneighborhood, const bool pDoubleElementsStorageCount);
 };
 #endif // INVERSE_INDEX_BLOOMIER_FILTER_H
