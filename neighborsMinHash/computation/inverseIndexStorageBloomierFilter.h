f6dfd142847c2a32bf9294ef63e3b53b77662004
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 17:59:48 +0100

    defintion to use as inverse index storage a vector with bloomier filters

---
+++ neighborsMinHash/computation/inverseIndexStorageBloomierFilter.h
@@ -0,0 +1,16 @@
+#include "inverseIndexStorage.h"
+#include "bloomierFilter/bloomierFilter.h"
+
+#ifndef INVERSE_INDEX_STORAGE_BLOOMIER_FILTER_H
+#define INVERSE_INDEX_STORAGE_BLOOMIER_FILTER_H
+class InverseIndexStorageBloomierFilter : public InverseIndexStorage {
+  private:
+	std::vector<BloomierFilter> mInverseIndex;
+  public:
+    InverseIndexStorageBloomierFilter(size_t pSizeOfInverseIndex);
+	~InverseIndexStorageBloomierFilter();
+  	size_t size();
+	vsize_t getElement(size_t pVectorId, size_t pHashValue);
+	void insert(size_t pVectorId, size_t pHashValue);
+};
+#endif // INVERSE_INDEX_STORAGE_BLOOMIER_FILTER_H
