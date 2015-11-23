7755eb968a42aae95b870b9251532b4f54110de5
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 18:25:48 +0100

    changed datatype to pointer

--- neighborsMinHash/computation/inverseIndexStorage.h
+++ neighborsMinHash/computation/inverseIndexStorage.h
@@ -5,6 +5,6 @@ class InverseIndexStorage {
     virtual InverseIndexStorage();
 	virtual ~InverseIndexStorage();
 	virtual size_t size();
-	virtual vsize_t getElement(size_t pVectorId, size_t pHashValue);
+	virtual vsize_t* getElement(size_t pVectorId, size_t pHashValue);
 	virtual void insert(size_t pVectorId, size_t pHashValue);
 }
