8e26bd09c91044c2df59dc41a2175d29ea50382e
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 16:46:36 +0100

    Added declaration of functions

--- neighborsMinHash/computation/inverseIndexUnorderedMap.h
+++ neighborsMinHash/computation/inverseIndexUnorderedMap.h
@@ -1,6 +1,7 @@
+#include "inverseIndex.h"
+
 #ifndef INVERSE_INDEX_UNORDERED_MAP_H
 #define INVERSE_INDEX_UNORDERED_MAP_H
-#include "inverseIndex.h"
 
 class InverseIndexUnorderedMap : public InverseIndex {
 	private: 
@@ -10,5 +11,10 @@ class InverseIndexUnorderedMap : public InverseIndex {
                     size_t pNumberOfCores, size_t pChunkSize,
                     size_t pMaxBinSize, size_t pMinimalBlocksInCommon,
                     size_t pExcessFactor, size_t pMaximalNumberOfHashCollisions);
+	 	~InverseIndexUnorderedMap();
+		vsize_t* computeSignature(const SparseMatrixFloat* pRawData, const size_t pInstance);
+  		umap_uniqueElement* computeSignatureMap(const SparseMatrixFloat* pRawData);
+  		void fit(const SparseMatrixFloat* pRawData);
+  		neighborhood* kneighbors(const umap_uniqueElement* signaturesMap, const int pNneighborhood, const bool pDoubleElementsStorageCount);
 };
 #endif // INVERSE_INDEX_UNORDERED_MAP_H
