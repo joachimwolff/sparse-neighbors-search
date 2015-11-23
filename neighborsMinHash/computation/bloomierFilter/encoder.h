801007215364337a10cb6b02bdbb7f2f48ae3c88
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 11:35:04 +0100

    Added masking vector and changed constructor

--- neighborsMinHash/computation/bloomierFilter/encoder.h
+++ neighborsMinHash/computation/bloomierFilter/encoder.h
@@ -2,9 +2,13 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 class Encoder {
+  private:
+	size_t mBitVectorSize;
+  vsize_t* mMaskingValues;
   public:
+  	Encoder(size_t pBitVectorSize);
   	bitVector* encode(size_t value);
-  	size_t decode(bitVector value);
+  	size_t decode(bitVector* value);
   	
 };
 #endif // ENCODER_H
