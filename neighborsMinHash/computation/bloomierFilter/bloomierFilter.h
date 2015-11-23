3cd7b3580b93a16ce42ba1b6bc0d2019a6832064
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 14:18:43 +0100

    Changed datype for value table

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
@@ -9,7 +9,7 @@
 class BloomierFilter {
   private:
     bloomierTable* mTable;
-    vsize_t* mValueTable;
+    vvsize_t_p* mValueTable;
     size_t mM;
     size_t mK;
     size_t mQ;
@@ -27,15 +27,15 @@ class BloomierFilter {
     ~BloomierFilter();
     
     size_t getByteSize(size_t pQ);
-    vsize_t* getTable();
-    void setTable(vsize_t pTable);
-    size_t getValueTable();
-    void setValueTable(vsize_t pTable);
-    void byteArrayXor(vsize_t* pResult, vsize_t* pInput);
-    size_t xorOperation(vsize_t* pValue, vsize_t* pM, vsize_t* pNeighbors);
-    vsize_t get(size_t pKey);
+    bloomierTable* getTable();
+    void setTable(bloomierTable* pTable);
+    vvsize_t_p* getValueTable();
+    void setValueTable(vvsize_t_p* pTable);
+    void xor(vsize_t* pResult, vsize_t* pInput);
+    void xorOperation(bitVector* pValue, bitVector* pM, vsize_t* pNeighbors);
+    vsize_t* get(size_t pKey);
     bool set(size_t pKey, size_t pValue);
-    void create(std::map<size_t, size_t> pAssignment);
+    void create(vsize_t* pKeys, vvsize_t_p* pValues);
     // std::string tableToString();
     // std::pair<vsize_t, vsize_t> stringToTable(std::string pString);
 };
