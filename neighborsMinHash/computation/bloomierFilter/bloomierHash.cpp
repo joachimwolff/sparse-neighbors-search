c0322b74c1fcd243ad8f3363c106dc13ea3482a9
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 13:55:28 +0100

    Implementation added

--- neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
+++ neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
@@ -1,18 +1,28 @@
 #include "bloomierHash.h"
 
-BloomierHash::BloomierHash(size_t pHashSeed, size_t pM, size_t pK, size_t pQ, size_t pByte) {
+BloomierHash::BloomierHash(size_t pM, size_t pK, size_t pBitVectorSize) {
+	mM = pM;
+	mK = pK;
+	// mQ = pQ;
+	mHash = new Hash();
+	mBitVectorSize = pBitVectorSize;
+};
+BloomierHash::~BloomierHash() {
 
-}
-
-size_t BloomierHash::getHashValue(size_t pKey) {
-
-}
-size_t BloomierHash::getM(size_t pKey) {
-
-}
-vsize_t* BloomierHash::getNeighborhood(size_t pKey){
-
-}
-vsize_t* BloomierHash::getM(size_t pKey){
-
-}
+};
+bitVector* BloomierHash::getMask(size_t pKey) {
+	bitVector* mask = new bitVector(mBitVectorSize);
+	for (size_t i = 0; i < mBitVectorSize; ++i) {
+		srand(pKey);
+		(*mask)[i] =  static_cast<char>(rand() % 255);
+	}
+	return mask;
+};
+vsize_t* BloomierHash::getKNeighbors(size_t pElement, size_t pK, size_t pModulo) {
+	
+	vsize_t* kNeighbors = new vsize_t(pK);
+	for (size_t i = 0; i < pK; ++i) {
+		(*kNeighbors)[i] = mHash->hash(pElement+1, pModulo, i+1);
+	}
+	return kNeighbors;
+};
