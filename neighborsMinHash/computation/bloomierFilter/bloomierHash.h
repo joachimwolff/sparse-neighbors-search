865a455873f1d395ea560902088e7ba76a35ab61
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-23 13:55:07 +0100

    Moved implementation to cpp and only defintion now in the header

--- neighborsMinHash/computation/bloomierFilter/bloomierHash.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierHash.h
@@ -17,31 +17,9 @@ class BloomierHash {
     size_t mBitVectorSize;
 
   public:      
-    BloomierHash(size_t pM, size_t pK, size_t pBitVectorSize) {
-        mM = pM;
-        mP = pK;
-        mQ = pQ;
-        mHash = new Hash();
-        mBitVectorSize = pBitVectorSize;
-    };
-    ~BloomierHash() {
-
-    };
-    bitVector* getMask(size_t pKey) {
-        bitVector* mask = new bitVector(mBitVectorSize);
-        for (size_t i = 0; i < mBitVectorSize; ++i) {
-            mask[i] =  static_cast<char>(rand(pKey) % 255);
-        }
-        return mask;
-    };
-    vsize_t* getKNeighbors(size_t pElement, size_t pK, size_t pModulo) {
-        
-        vsize_t* kNeighbors = new vsize_t(pK);
-        for (size_t i = 0; i < pK; ++i) {
-            (*kNeighbors)[i] = mHash->hash(pElement+1, pModulo, i+1);
-        }
-        return kNeighbors;
-    };
-
+    BloomierHash(size_t pM, size_t pK, size_t pBitVectorSize);
+    ~BloomierHash();
+    bitVector* getMask(size_t pKey);
+    vsize_t* getKNeighbors(size_t pElement, size_t pK, size_t pModulo);
 };
 #endif // BLOOMIER_HASH_H
