6d92524ece3ccca2915bf2feb9827b10e428447f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-24 15:12:59 +0100

    Added a parameter to function insert

--- neighborsMinHash/computation/inverseIndexStorage.h
+++ neighborsMinHash/computation/inverseIndexStorage.h
@@ -1,10 +1,13 @@
 #include "typeDefinitions.h"
-
+#ifndef INVERSE_INDEX_STORAGE_H
+#define INVERSE_INDEX_STORAGE_H
 class InverseIndexStorage {
   public:
-    virtual InverseIndexStorage();
+    InverseIndexStorage();
 	virtual ~InverseIndexStorage();
 	virtual size_t size();
 	virtual vsize_t* getElement(size_t pVectorId, size_t pHashValue);
-	virtual void insert(size_t pVectorId, size_t pHashValue);
-}
+	virtual void insert(size_t pVectorId, size_t pHashValue, size_t pInstance);
+	// virtual void create();
+};
+#endif // INVERSE_INDEX_STORAGE_H
