3ff3548a2713596b97ea7212a711a467baba013d
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-24 15:13:39 +0100

    Added parameter to function insert and changed mInverseIndex to pointer

--- neighborsMinHash/computation/inverseIndexStorageBloomierFilter.h
+++ neighborsMinHash/computation/inverseIndexStorageBloomierFilter.h
@@ -5,12 +5,19 @@
 #define INVERSE_INDEX_STORAGE_BLOOMIER_FILTER_H
 class InverseIndexStorageBloomierFilter : public InverseIndexStorage {
   private:
-	std::vector<BloomierFilter> mInverseIndex;
+	std::vector<BloomierFilter* >* mInverseIndex;
+	vvsize_t* mKeys;
+	vvsize_t* mValues;
+	size_t mMaxBinSize;
+	size_t mM;
+	size_t mK;
+	size_t mQ;
   public:
-    InverseIndexStorageBloomierFilter(size_t pSizeOfInverseIndex);
+    InverseIndexStorageBloomierFilter(size_t pSizeOfInverseIndex, size_t pMaxBinSize);
 	~InverseIndexStorageBloomierFilter();
   	size_t size();
 	vsize_t* getElement(size_t pVectorId, size_t pHashValue);
-	void insert(size_t pVectorId, size_t pHashValue);
+	void insert(size_t pVectorId, size_t pHashValue, size_t pInstance);
+	// void create();
 };
 #endif // INVERSE_INDEX_STORAGE_BLOOMIER_FILTER_H
