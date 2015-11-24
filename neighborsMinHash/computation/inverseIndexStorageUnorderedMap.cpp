20c527ea6dc1ef9a07849d653e654717547f1158
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-24 15:15:50 +0100

    Fixed copy&paste errors of wrong variable names

--- neighborsMinHash/computation/inverseIndexStorageUnorderedMap.cpp
+++ neighborsMinHash/computation/inverseIndexStorageUnorderedMap.cpp
@@ -4,6 +4,8 @@
 InverseIndexStorageUnorderedMap::InverseIndexStorageUnorderedMap(size_t pSizeOfInverseIndex, size_t pMaxBinSize) {
 	mSignatureStorage = new vector__umapVector(pSizeOfInverseIndex);
 	mMaxBinSize = pMaxBinSize;
+	mKeys = new vvsize_t(pSizeOfInverseIndex, vsize_t());
+	mValues = new vvsize_t(pSizeOfInverseIndex, vsize_t());
 }
 InverseIndexStorageUnorderedMap::~InverseIndexStorageUnorderedMap() {
 	
@@ -12,8 +14,8 @@ size_t InverseIndexStorageUnorderedMap::size() {
 	return mSignatureStorage->size();
 }
 vsize_t* InverseIndexStorageUnorderedMap::getElement(size_t pVectorId, size_t pHashValue) {
-	auto iterator = (*mSignatureStorage)[pVectorId]->find(pHashValue);
-	if (iterator != (*mSignatureStorage)[pVectorId]->end()) {
+	auto iterator = (*mSignatureStorage)[pVectorId].find(pHashValue);
+	if (iterator != (*mSignatureStorage)[pVectorId].end()) {
 		return &(iterator->second);
 	}
 	return new vsize_t(0);
@@ -25,14 +27,14 @@ vsize_t* InverseIndexStorageUnorderedMap::getElement(size_t pVectorId, size_t pH
     //                 if (itHashValue_InstanceVector->second.size()
 }
 void InverseIndexStorageUnorderedMap::insert(size_t pVectorId, size_t pHashValue, size_t pInstance) {
-	 auto itHashValue_InstanceVector = (*mSignatureStorage)[j].find((*signature)[j]);
+	auto itHashValue_InstanceVector = (*mSignatureStorage)[pVectorId].find(pHashValue);
 	// if for hash function h_i() the given hash values is already stored
-	if (itHashValue_InstanceVector != (*mSignatureStorage)[j].end()) {
+	if (itHashValue_InstanceVector != (*mSignatureStorage)[pVectorId].end()) {
 		// insert the instance id if not too many collisions (maxBinSize)
 		if (itHashValue_InstanceVector->second.size() < mMaxBinSize) {
 			// insert only if there wasn't any collisions in the past
 			if (itHashValue_InstanceVector->second.size() > 0) {
-				itHashValue_InstanceVector->second.push_back(index);
+				itHashValue_InstanceVector->second.push_back(pInstance);
 			}
 		} else { 
 			// too many collisions: delete stored ids. empty vector is interpreted as an error code 
@@ -42,7 +44,11 @@ void InverseIndexStorageUnorderedMap::insert(size_t pVectorId, size_t pHashValue
 	} else {
 		// given hash value for the specific hash function was not avaible: insert new hash value
 		vsize_t instanceIdVector;
-		instanceIdVector.push_back(index);
-		(*mSignatureStorage)[j][(*signature)[j]] = instanceIdVector;
+		instanceIdVector.push_back(pInstance);
+		(*mSignatureStorage)[pVectorId][pHashValue] = instanceIdVector;
 	}
 }
+
+// void InverseIndexStorageUnorderedMap::create() {
+
+// }
