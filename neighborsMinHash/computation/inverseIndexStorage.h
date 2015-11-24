66ea398d8aa0676786772dba83b2dd56e0bc2724
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-11-24 16:48:20 +0100

    Defined class as abstract

--- neighborsMinHash/computation/inverseIndexStorage.h
+++ neighborsMinHash/computation/inverseIndexStorage.h
@@ -3,11 +3,11 @@
 #define INVERSE_INDEX_STORAGE_H
 class InverseIndexStorage {
   public:
-    InverseIndexStorage();
-	virtual ~InverseIndexStorage();
-	virtual size_t size();
-	virtual vsize_t* getElement(size_t pVectorId, size_t pHashValue);
-	virtual void insert(size_t pVectorId, size_t pHashValue, size_t pInstance);
+    // InverseIndexStorage();
+	// virtual ~InverseIndexStorage();
+	virtual size_t size() = 0;
+	virtual vsize_t* getElement(size_t pVectorId, size_t pHashValue) = 0;
+	virtual void insert(size_t pVectorId, size_t pHashValue, size_t pInstance) = 0;
 	// virtual void create();
 };
 #endif // INVERSE_INDEX_STORAGE_H
