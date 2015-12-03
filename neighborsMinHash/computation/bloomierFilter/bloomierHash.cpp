ae80294a795140a6aafa06e35f2a97d885aaa18f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-03 14:15:13 +0100

    Added hashSeed as memeber variable + get&set

--- neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
+++ neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
@@ -1,18 +1,19 @@
 #include "bloomierHash.h"
 #include <set>
-BloomierHash::BloomierHash(size_t pModulo,size_t pNumberOfElements, size_t pBitVectorSize) {
+BloomierHash::BloomierHash(size_t pModulo,size_t pNumberOfElements, size_t pBitVectorSize, size_t pHashSeed) {
 	mModulo = pModulo;
 	mNumberOfElements = pNumberOfElements;
 	// mQ = pQ;
 	mHash = new Hash();
 	mBitVectorSize = pBitVectorSize;
+	mHashSeed = pHashSeed;
 };
 BloomierHash::~BloomierHash() {
 
 };
 bitVector* BloomierHash::getMask(size_t pKey) {
 	bitVector* mask = new bitVector(mBitVectorSize);
-	srand(pKey);
+	srand(mHashSeed);
 	size_t randValue = rand();
 	for (size_t i = 0; i < mBitVectorSize; ++i) {
 		(*mask)[i] = randValue >> (i*8);
@@ -24,9 +25,17 @@ vsize_t* BloomierHash::getKNeighbors(size_t pElement) {
 	// std::set<size_t>* setOfNeighbors = new std::set<size_t>();
 	// srand(pElement);
 	for (size_t i = 0; i < mNumberOfElements; ++i) {
-		size_t neighbor = mHash->hash(pElement+1, rand(), mModulo);
+		size_t neighbor = mHash->hash(pElement+1, mHashSeed, mModulo);
 		(*kNeighbors)[i] = neighbor;
 	}
 	// delete setOfNeighbors;
 	return kNeighbors;
 };
+
+size_t BloomierHash::getHashSeed() {
+	return mHashSeed;
+};
+
+void BloomierHash::setHashSeed(size_t pHashSeed) {
+	mHashSeed = pHashSeed;
+}
