c208b69dddcaab299f466b98846747d7d6a3ce16
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-03 14:06:44 +0100

    Added hashSeed as memeber variable + get&set

--- neighborsMinHash/computation/bloomierFilter/bloomierHash.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierHash.h
@@ -13,11 +13,14 @@ class BloomierHash {
     size_t mNumberOfElements;
     Hash* mHash;
     size_t mBitVectorSize;
+    size_t mHashSeed;
 
   public:      
-    BloomierHash(size_t pModulo, size_t pNumberOfElements, size_t pBitVectorSize);
+    BloomierHash(size_t pModulo, size_t pNumberOfElements, size_t pBitVectorSize, size_t pHashSeed);
     ~BloomierHash();
     bitVector* getMask(size_t pKey);
     vsize_t* getKNeighbors(size_t pElement);
+    size_t getHashSeed();
+    void setHashSeed(size_t pHashSeed);
 };
 #endif // BLOOMIER_HASH_H
