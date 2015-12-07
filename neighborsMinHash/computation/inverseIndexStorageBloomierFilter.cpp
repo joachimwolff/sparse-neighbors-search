42404fbdca58da161fbae29968a4dbdc98407bdb
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-07 20:30:05 +0100

    Do not store values in map and create everything in one batch. introduce values direct in the bloomierFilter

--- neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
+++ neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
@@ -22,38 +22,42 @@ vsize_t* InverseIndexStorageBloomierFilter::getElement(size_t pVectorId, size_t 
     return (*mInverseIndex)[pVectorId]->get(pHashValue); 
 }
 void InverseIndexStorageBloomierFilter::insert(size_t pVectorId, size_t pHashValue, size_t pInstance) {
+    if ((*mInverseIndex)[pVectorId] == NULL) {
+        (*mInverseIndex)[pVectorId] = new BloomierFilter(mM, mK, mQ, 100);
+    }
+    (*mInverseIndex)[pVectorId]->set(pHashValue, pInstance);
 	// (*mKeys)[pVectorId]->push_back(pHashValue);
-   auto itHashValue_InstanceVector = (*mValues)[pVectorId].find(pHashValue);
-	// if for hash function h_i() the given hash values is already stored
-	if (itHashValue_InstanceVector != (*mValues)[pVectorId].end()) {
-		// insert the instance id if not too many collisions (maxBinSize)
-		if (itHashValue_InstanceVector->second.size() < mMaxBinSize) {
-			// insert only if there wasn't any collisions in the past
-			if (itHashValue_InstanceVector->second.size() > 0) {
-				itHashValue_InstanceVector->second.push_back(pInstance);
-			}
-		} else { 
-			// too many collisions: delete stored ids. empty vector is interpreted as an error code 
-			// for too many collisions
-			itHashValue_InstanceVector->second.clear();
-		}
-	} else {
-		// given hash value for the specific hash function was not avaible: insert new hash value
-		vsize_t instanceIdVector;
-		instanceIdVector.push_back(pInstance);
-		(*mValues)[pVectorId][pHashValue] = instanceIdVector;
-	}
+//    auto itHashValue_InstanceVector = (*mValues)[pVectorId].find(pHashValue);
+// 	// if for hash function h_i() the given hash values is already stored
+// 	if (itHashValue_InstanceVector != (*mValues)[pVectorId].end()) {
+// 		// insert the instance id if not too many collisions (maxBinSize)
+// 		if (itHashValue_InstanceVector->second.size() < mMaxBinSize) {
+// 			// insert only if there wasn't any collisions in the past
+// 			if (itHashValue_InstanceVector->second.size() > 0) {
+// 				itHashValue_InstanceVector->second.push_back(pInstance);
+// 			}
+// 		} else { 
+// 			// too many collisions: delete stored ids. empty vector is interpreted as an error code 
+// 			// for too many collisions
+// 			itHashValue_InstanceVector->second.clear();
+// 		}
+// 	} else {
+// 		// given hash value for the specific hash function was not avaible: insert new hash value
+// 		vsize_t instanceIdVector;
+// 		instanceIdVector.push_back(pInstance);
+// 		(*mValues)[pVectorId][pHashValue] = instanceIdVector;
+// 	}
 }
 
 void InverseIndexStorageBloomierFilter::create() {
-	for (size_t i = 0; i < mValues->size(); ++i) {
-		vsize_t* keys = new vsize_t((*mValues)[i].size());
-            std::cout << "size of keys: " << (*mValues)[i].size() << std::endl;
+	// for (size_t i = 0; i < mValues->size(); ++i) {
+	// 	vsize_t* keys = new vsize_t((*mValues)[i].size());
+    //         std::cout << "size of keys: " << (*mValues)[i].size() << std::endl;
 
-		for (auto it = (*mValues)[i].begin(); it != (*mValues)[i].end(); ++it) {
-	        (*keys)[i] = it->first;
-    	}
-		(*mInverseIndex)[i] = new BloomierFilter(mM, mK, mQ, keys, 100);
-        (*mInverseIndex)[i]->create(&(*mValues)[i], 0 );
-    }
+	// 	for (auto it = (*mValues)[i].begin(); it != (*mValues)[i].end(); ++it) {
+	//         (*keys)[i] = it->first;
+    // 	}
+	// 	(*mInverseIndex)[i] = new BloomierFilter(mM, mK, mQ, keys, 100);
+    //     (*mInverseIndex)[i]->create(&(*mValues)[i], 0 );
+    // }
 }
