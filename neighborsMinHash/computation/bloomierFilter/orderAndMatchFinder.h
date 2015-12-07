d56fd165632ce921d0e5937e661222a82bfa2499
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-07 20:29:13 +0100

    Introduced Bloom filter for a few memebers

--- neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
+++ neighborsMinHash/computation/bloomierFilter/orderAndMatchFinder.h
@@ -9,21 +9,24 @@ class OrderAndMatchFinder {
   private:
     size_t mModulo;
     size_t mNumberOfElements;
-    
+    size_t mBloomFilterInstance;
+    size_t mBloomFilterInstanceDifferentSeed;
+    size_t mBloomFilterHashesSeen;
+    size_t mBloomFilterNonSingeltons;
+    size_t mBloomFilterSeed;
     vsize_t* mPiVector;
     vsize_t* mTauVector;
-    std::set<size_t>* mHashesSeen;
-    std::set<size_t>* mNonSingeltons;
+    
     BloomierHash* mBloomierHash;
     std::unordered_map<size_t, size_t>* mSeeds;
-    
+    Hash* mHash;
     bool findMatch(vsize_t* pSubset);
     FRIEND_TEST(OrderAndMatchFinderTest, computeNonSingeltonsTest);
     void computeNonSingeltons(vsize_t* pKeyValues, size_t pSeed = 0);
     FRIEND_TEST(OrderAndMatchFinderTest, tweakTest);
     int tweak(size_t pKey, vsize_t* pSubset, size_t pSeed = 0);
   public:
-  	OrderAndMatchFinder(size_t pModulo, size_t pNumberOfElements, vsize_t* pSubset, BloomierHash* pBloomierHash);
+  	OrderAndMatchFinder(size_t pModulo, size_t pNumberOfElements, BloomierHash* pBloomierHash);
     ~OrderAndMatchFinder();
     void find(vsize_t* pSubset);
     vsize_t* getPiVector();
