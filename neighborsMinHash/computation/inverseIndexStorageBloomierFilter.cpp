9d0d2104341d4c6e2be535d9fb5e3913b3b3ca9b
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-08 16:05:09 +0100

    Added maxBinSize to bloomieFilter constructor

--- neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
+++ neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
@@ -3,7 +3,7 @@
 InverseIndexStorageBloomierFilter::InverseIndexStorageBloomierFilter(size_t pSizeOfInverseIndex, size_t pMaxBinSize) {
 	mInverseIndex = new std::vector<BloomierFilter* > (pSizeOfInverseIndex);
 	mMaxBinSize = pMaxBinSize;
-	mM = 5000;
+	mM = 10000;
 	mK = 5;
 	mQ = 8;
 }
@@ -18,7 +18,7 @@ vsize_t* InverseIndexStorageBloomierFilter::getElement(size_t pVectorId, size_t 
 }
 void InverseIndexStorageBloomierFilter::insert(size_t pVectorId, size_t pHashValue, size_t pInstance) {
     if ((*mInverseIndex)[pVectorId] == NULL) {
-        (*mInverseIndex)[pVectorId] = new BloomierFilter(mM, mK, mQ, 100);
+        (*mInverseIndex)[pVectorId] = new BloomierFilter(mM, mK, mQ, 100, mMaxBinSize);
     }
     (*mInverseIndex)[pVectorId]->set(pHashValue, pInstance);
 }
