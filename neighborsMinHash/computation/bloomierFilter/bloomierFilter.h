a3a5d247b9c20d1e77cb077e38e62b860bdb619e
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-08 19:21:04 +0100

    Renamed member variables; deleted unused and unnecessary functions. Changed parameters for function create to decrease complexity

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
@@ -9,10 +9,10 @@
 class BloomierFilter {
   private:
     bloomierTable* mTable;
-    vvsize_t* mValueTable;
-    size_t mM;
-    size_t mK;
-    size_t mQ;
+    vvsize_t_p* mValueTable;
+    size_t mModulo;
+	size_t mNumberOfElements;
+    // size_t mQ;
     size_t mHashSeed;
     BloomierHash* mBloomierHash;
     size_t mBitVectorSize;
@@ -21,20 +21,21 @@ class BloomierFilter {
     size_t mPiIndex;
     size_t mMaxBinSize;
 
+    void create( size_t pKey, size_t pValue);
+    
   public:      
     BloomierFilter(size_t pM, size_t pK, size_t pQ, size_t pHashSeed, size_t pMaxBinSize);
     
     ~BloomierFilter();
     
     size_t getByteSize(size_t pQ);
-    bloomierTable* getTable();
-    void setTable(bloomierTable* pTable);
-    vvsize_t* getValueTable();
-    void setValueTable(vvsize_t* pTable);
+    // bloomierTable* getTable();
+    // void setTable(bloomierTable* pTable);
+    // vvsize_t* getValueTable();
+    // void setValueTable(vvsize_t* pTable);
     void xorBitVector(bitVector* pResult, bitVector* pInput);
     void xorOperation(bitVector* pValue, bitVector* pM, vsize_t* pNeighbors);
     vsize_t* get(size_t pKey);
     bool set(size_t pKey, size_t pValue);
-    void create( std::unordered_map<size_t, vsize_t >* pKeyValue, size_t piIndex = 0);
 };
 #endif // BLOOMIER_FILTER_H
