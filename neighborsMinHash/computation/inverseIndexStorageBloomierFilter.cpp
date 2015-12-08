490b01e3179277217ed46e04af637bf3b0662461
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-08 14:55:04 +0100

    Removed dead code

--- neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
+++ neighborsMinHash/computation/inverseIndexStorageBloomierFilter.cpp
@@ -3,11 +3,6 @@
 InverseIndexStorageBloomierFilter::InverseIndexStorageBloomierFilter(size_t pSizeOfInverseIndex, size_t pMaxBinSize) {
 	mInverseIndex = new std::vector<BloomierFilter* > (pSizeOfInverseIndex);
 	mMaxBinSize = pMaxBinSize;
-	// mKeys = new vvsize_t(pSizeOfInverseIndex, vsize_t());
-	mValues = new vector__umapVector(pSizeOfInverseIndex);
-
-
-	// mValues = new std::vector< vvsize_t>(pSizeOfInverseIndex, vvsize_t());
 	mM = 5000;
 	mK = 5;
 	mQ = 8;
@@ -26,38 +21,4 @@ void InverseIndexStorageBloomierFilter::insert(size_t pVectorId, size_t pHashVal
         (*mInverseIndex)[pVectorId] = new BloomierFilter(mM, mK, mQ, 100);
     }
     (*mInverseIndex)[pVectorId]->set(pHashValue, pInstance);
-	// (*mKeys)[pVectorId]->push_back(pHashValue);
-//    auto itHashValue_InstanceVector = (*mValues)[pVectorId].find(pHashValue);
-// 	// if for hash function h_i() the given hash values is already stored
-// 	if (itHashValue_InstanceVector != (*mValues)[pVectorId].end()) {
-// 		// insert the instance id if not too many collisions (maxBinSize)
-// 		if (itHashValue_InstanceVector->second.size() < mMaxBinSize) {
-// 			// insert only if there wasn't any collisions in the past
-// 			if (itHashValue_InstanceVector->second.size() > 0) {
-// 				itHashValue_InstanceVector->second.push_back(pInstance);
-// 			}
-// 		} else { 
-// 			// too many collisions: delete stored ids. empty vector is interpreted as an error code 
-// 			// for too many collisions
-// 			itHashValue_InstanceVector->second.clear();
-// 		}
-// 	} else {
-// 		// given hash value for the specific hash function was not avaible: insert new hash value
-// 		vsize_t instanceIdVector;
-// 		instanceIdVector.push_back(pInstance);
-// 		(*mValues)[pVectorId][pHashValue] = instanceIdVector;
-// 	}
-}
-
-void InverseIndexStorageBloomierFilter::create() {
-	// for (size_t i = 0; i < mValues->size(); ++i) {
-	// 	vsize_t* keys = new vsize_t((*mValues)[i].size());
-    //         std::cout << "size of keys: " << (*mValues)[i].size() << std::endl;
-
-	// 	for (auto it = (*mValues)[i].begin(); it != (*mValues)[i].end(); ++it) {
-	//         (*keys)[i] = it->first;
-    // 	}
-	// 	(*mInverseIndex)[i] = new BloomierFilter(mM, mK, mQ, keys, 100);
-    //     (*mInverseIndex)[i]->create(&(*mValues)[i], 0 );
-    // }
 }
