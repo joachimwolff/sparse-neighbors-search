044facb9624d8e92e9eeb89e2a8112693036c662
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-10 16:58:50 +0100

    Extended destructor, reduced complexity of seed computation in getKNeighbors

--- neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
+++ neighborsMinHash/computation/bloomierFilter/bloomierHash.cpp
@@ -1,6 +1,6 @@
 #include "bloomierHash.h"
-#include <set>
-#include <bitset>
+// #include <set>
+// #include <bitset>
 BloomierHash::BloomierHash(const size_t pModulo, const size_t pNumberOfElements, const size_t pBitVectorSize, const size_t pHashSeed) {
 	mModulo = pModulo;
 	mNumberOfElements = pNumberOfElements;
@@ -9,15 +9,14 @@ BloomierHash::BloomierHash(const size_t pModulo, const size_t pNumberOfElements,
 	mHashSeed = pHashSeed;
 };
 BloomierHash::~BloomierHash() {
-
+    delete mHash;
 };
 bitVector* BloomierHash::getMask(const size_t pKey) {
 	bitVector* mask = new bitVector(mBitVectorSize);
 	size_t randValue = mHash->hash(pKey+1, mHashSeed, mModulo);
 	for (size_t i = 0; i < mBitVectorSize; ++i) {
-		(*mask)[i] =  static_cast<unsigned char>((randValue >> (8*i))& 0b00000000000000000000000011111111);
-	} 
-	
+		(*mask)[i] =  static_cast<unsigned char>((randValue >> (8*i)) & 0b00000000000000000000000011111111);
+	}
 	return mask;
 };
 void BloomierHash::getKNeighbors(const size_t pElement, const size_t pSeed, vsize_t* pNeighbors) {
@@ -34,7 +33,7 @@ void BloomierHash::getKNeighbors(const size_t pElement, const size_t pSeed, vsiz
         unsigned char valueSeen = (*bloomFilterValueSeen)[index] & value;
         while (value == valueSeen) {
             ++seedChange;
-            neighbor = mHash->hash(pElement+1, (seedValue+seedChange)*(seedValue+seedChange), mModulo);
+            neighbor = mHash->hash(pElement+1, (seedValue+seedChange), mModulo);
             index = floor(neighbor / 8.0);
             value = 1 << (neighbor % 8);
             valueSeen = (*bloomFilterValueSeen)[index] & value;
