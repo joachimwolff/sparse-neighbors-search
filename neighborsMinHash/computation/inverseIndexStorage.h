c3a48f30ab9736f3ea4517a6d2f4c388da7a56d0
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-11 15:31:29 +0100

    Defined return type of function getElement to const

--- neighborsMinHash/computation/inverseIndexStorage.h
+++ neighborsMinHash/computation/inverseIndexStorage.h
@@ -4,7 +4,7 @@
 class InverseIndexStorage {
   public:
 	virtual size_t size() const = 0;
-	virtual vsize_t* getElement(size_t pVectorId, size_t pHashValue) = 0;
+	virtual const vsize_t* getElement(size_t pVectorId, size_t pHashValue) = 0;
 	virtual void insert(size_t pVectorId, size_t pHashValue, size_t pInstance) = 0;
 };
 #endif // INVERSE_INDEX_STORAGE_H
