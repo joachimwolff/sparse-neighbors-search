8f9d621416bf196d89361359e4ec8e2bca7c37c1
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2015-12-11 15:23:09 +0100

    Deleted unused code and made return type of get const

--- neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
+++ neighborsMinHash/computation/bloomierFilter/bloomierFilter.h
@@ -12,7 +12,6 @@ class BloomierFilter {
     vvsize_t_p* mValueTable;
     size_t mModulo;
 	size_t mNumberOfElements;
-    // size_t mQ;
     size_t mHashSeed;
     BloomierHash* mBloomierHash;
     size_t mBitVectorSize;
@@ -21,7 +20,6 @@ class BloomierFilter {
     size_t mPiIndex;
     size_t mMaxBinSize;
     std::unordered_map<size_t, vsize_t* >* mStoredNeighbors;
-    // std::unordered_map<size_t, bitVector*>*  mStoredMasks;
     void create( size_t pKey, size_t pValue);
     
   public:      
@@ -30,13 +28,9 @@ class BloomierFilter {
     ~BloomierFilter();
     
     size_t getByteSize(const size_t pQ);
-    // bloomierTable* getTable();
-    // void setTable(bloomierTable* pTable);
-    // vvsize_t* getValueTable();
-    // void setValueTable(vvsize_t* pTable);
     void xorBitVector(bitVector* pResult, const bitVector* pInput);
     void xorOperation(bitVector* pValue, const bitVector* pM, const vsize_t* pNeighbors);
-    vsize_t* get(const size_t pKey);
+    const vsize_t* get(const size_t pKey);
     bool set(const size_t pKey, const size_t pValue);
 };
 #endif // BLOOMIER_FILTER_H
