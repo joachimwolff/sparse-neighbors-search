5a1c42855210695a70140ec502bd242384c7af30
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-01-26 20:01:37 +0100

    helper script for benchmarking

---
+++ getSizeOfHashFunctions.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+# Copyright 2016 Joachim Wolff
+# Master Thesis
+# Tutors: Milad Miladi, Fabrizio Costa
+# Winter semester 2015/2016
+#
+# Chair of Bioinformatics
+# Department of Computer Science
+# Faculty of Engineering
+# Albert-Ludwig-University Freiburg im Breisgau
+#
+__author__ = 'wolffjoachim'
+
+from neighborsMinHash import MinHash
+from sklearn.neighbors import NearestNeighbors
+
+import time
+import os.path
+import cPickle as pickle
+from sklearn.metrics import accuracy_score
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+   
+    parser.add_argument('-i', '--input_file_name', type=str,dest="input_file_name")
+    parser.add_argument('-o', '--output_file_name', type=str,dest="output_file_name")
+    
+    args = parser.parse_args()
+    input_file_name = args.input_file_name
+    output_file_name = args.output_file_name
+    
+    
+    if os.path.isfile(input_file_name):
+        fileObject = open(input_file_name,'r')
+        data = pickle.load(fileObject)
+        elements_in_index = []
+        for i in data[0][0].iterkeys():
+            elements_in_index.append(i)
+        fileObjectOut = open(output_file_name,'wb')
+        # pickle.dump(max_value, fileObjectOut)
+        for i in xrange(len(elements_in_index)):
+            fileObjectOut.write(str(elements_in_index[i])+" ")
+        fileObjectOut.close()
