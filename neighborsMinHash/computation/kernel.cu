1f6a32d6e47c0386fc973d92bcdaa25a44038cfc
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-02-03 18:11:37 +0100

    Reorganized code, commented unused stuff out and fixed some typos

--- neighborsMinHash/computation/kernel.cu
+++ neighborsMinHash/computation/kernel.cu
@@ -1,35 +1,10 @@
 #include <stdio.h>
 #include "sparseMatrix.h"
-#include <math.h>
-#define float A = sqrt(2) - 1;
+// #include <math.h>
+#include "kernel.h"
 
-__global__ void fit(size_t* pFeatureIdList, size_t* pSizeOfInstanceList, size_t pNumberOfHashFunctions, size_t pMaxNnz
-                        size_t* pComputedSignatures) {
-    __shared__ size_t signature[pNumberOfHashFunctions];
-    size_t threadId = threadIdx.x;
-    size_t blockId = blockIdx.x;
-    size_t minHashValue = MAX_VALUE;
-    size_t hashValue = 0;
-    if (blockId < pRawData->size()) {
-        // size_t sizeOfInstance = pSizeOfInstanceList[blockId];
-        for (size_t i = 0; i < pSizeOfInstanceList[blockId]; ++i) {
-            hashValue = computeHashValue((pFeatureIdList[blockId*pMaxNnz + i] + 1)*threadId, MAX_VALUE);
-            if (hashValue < minHashValue) {
-                minHashValue = hashValue;
-            }
-        }
-        signature[threadId] = minHashValue;
-    }
-    __syncthread();
-    
-    if (threadId == 0) {
-        for (size_t i = 0; i < pNumberOfHashFunctions; ++i) {
-            pComputedSignatures[blockId*pNumberOfHashFunctions +i] = signature[i];
-        }
-    }
-}
 
-__device__ size_t computeHashValue(size_t key, size_t aModulo) {
+__device__ size_t computeHashValueGpu(size_t key, size_t aModulo) {
     // source:  Thomas Wang: Integer Hash Functions, 1997 / 2007 
     // https://gist.github.com/badboy/6267743
     key = key * A;
@@ -42,12 +17,42 @@ __device__ size_t computeHashValue(size_t key, size_t aModulo) {
     return key % aModulo;
 }
 
-__global__ void query(size_t** pSignature, size_t** pInverseIndex, size_t* pElementsPerHashFunction, size_t pNumberOfHashFunctions,
+__global__ void fitGpu(const size_t* pFeatureIdList, const size_t* pSizeOfInstanceList,
+                    const size_t pNumberOfHashFunctions, const size_t pMaxNnz,
+                    size_t* pComputedSignatures, 
+                    const size_t pNumberOfInstances) {
+    extern __shared__ size_t signature[];  // pNumberOfHashFunctions
+    size_t threadId = threadIdx.x;
+    size_t blockId = blockIdx.x;
+    size_t minHashValue = MAX_VALUE;
+    size_t hashValue = 0;
+    if (blockId < pNumberOfInstances*pMaxNnz) {
+        // size_t sizeOfInstance = pSizeOfInstanceList[blockId];
+        for (size_t i = 0; i < pSizeOfInstanceList[blockId]; ++i) {
+            hashValue = computeHashValueGpu((pFeatureIdList[blockId*pMaxNnz + i] + 1)*threadId, MAX_VALUE);
+            if (hashValue < minHashValue) {
+                minHashValue = hashValue;
+            }
+        }
+        signature[threadId] = minHashValue;
+    }
+    __syncthreads();
+    
+    if (threadId == 0) {
+        for (size_t i = 0; i < pNumberOfHashFunctions; ++i) {
+            pComputedSignatures[blockId*pNumberOfHashFunctions +i] = signature[i];
+        }
+    }
+}
+
+
+
+__global__ void queryGpu(size_t** pSignature, size_t** pInverseIndex, size_t* pElementsPerHashFunction, size_t pNumberOfHashFunctions,
                         size_t* pHitsToBeReturned) {
     size_t threadId = threadIdx.x;
     size_t blockId = blockIdx.x;
     size_t hashValue = pSignature[blockId][threadId];
-    __shared__ size_t hits[pNumberOfHashFunctions];
+    extern __shared__ size_t hits[]; // pNumberOfHashFunctions
     
     for (size_t i = 0; i < pElementsPerHashFunction[threadId]; ++i) {
         if (hashValue == pInverseIndex[threadId][i]) {
@@ -57,7 +62,7 @@ __global__ void query(size_t** pSignature, size_t** pInverseIndex, size_t* pElem
             continue;
         }
     }
-    __syncthread();
+    __syncthreads();
     
     if (threadId == 0) {
         for (size_t i = 0; i < pNumberOfHashFunctions; ++i) {
@@ -66,7 +71,7 @@ __global__ void query(size_t** pSignature, size_t** pInverseIndex, size_t* pElem
     }  
 }
 
-__global__ void euclidianDistance(size_t* pFeatureIds, size_t* pSizeOfInstanceList,
+__global__ void euclidianDistanceGpu(size_t* pFeatureIds, size_t* pSizeOfInstanceList,
                                     float* pFeatureValues, size_t pMaxNnz,
                                     size_t* pPossibleInstances, size_t* pSizePerInstance, size_t pMaxCandidates,                             
                                     size_t* pHitsToBeReturned, float* pValuesToBeReturned) {
@@ -74,11 +79,11 @@ __global__ void euclidianDistance(size_t* pFeatureIds, size_t* pSizeOfInstanceLi
     size_t blockId = blockIdx.x;
     // size_t hashValue = pSignature[blockId][threadId];
     // __shared__ size_t hits[pNumberOfHashFunctions];
-    size_t startIndexInstance = *  
-    size_t endIndexInstance = 
+    // size_t startIndexInstance = *  
+    // size_t endIndexInstance = 
     
 }
 
-__global__ void cosineSimilarity() {
+__global__ void cosineSimilarityGpu() {
     
 }
