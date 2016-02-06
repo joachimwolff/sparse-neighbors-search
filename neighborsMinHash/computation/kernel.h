6d3e10b3ebd83e94b5cc4f0c92d196591a683760
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-02-06 22:38:05 +0100

    Renamed functions to end with cuda instead gpu. Added cuda specific keywords in the function definitions

--- neighborsMinHash/computation/kernel.h
+++ neighborsMinHash/computation/kernel.h
@@ -2,16 +2,17 @@
 #define KERNEL_CUDA
 #define A 0.414213562 // sqrt(2) - 1
 
-size_t computeHashValueGpu(size_t key, size_t aModulo);
-void fitGpu(const size_t* pFeatureIdList, const size_t* pSizeOfInstanceList,
+__device__ size_t computeHashValueCuda(size_t key, size_t aModulo);
+__global__ void fitCuda(const size_t* pFeatureIdList, const size_t* pSizeOfInstanceList,
                     const size_t pNumberOfHashFunctions, const size_t pMaxNnz,
                     size_t* pComputedSignatures, 
                     const size_t pNumberOfInstances);
-void queryGpu(size_t** pSignature, size_t** pInverseIndex, size_t* pElementsPerHashFunction, size_t pNumberOfHashFunctions,
+__global__ void queryCuda(size_t* pSignature, size_t* pInverseIndex, size_t pNumberOfHashFunctions, 
+                        size_t pNumberOfInstancs, size_t pSizeOfInverseIndex, size_t pSignatureCount,
                         size_t* pHitsToBeReturned);
-void euclidianDistanceGpu(size_t* pFeatureIds, size_t* pSizeOfInstanceList,
+__global__ void euclidianDistanceCuda(size_t* pFeatureIds, size_t* pSizeOfInstanceList,
                                     float* pFeatureValues, size_t pMaxNnz,
                                     size_t* pPossibleInstances, size_t* pSizePerInstance, size_t pMaxCandidates,                             
                                     size_t* pHitsToBeReturned, float* pValuesToBeReturned);
-void cosineSimilarityGpu();
+__global__ void cosineSimilarityCuda();
 #endif // KERNEL_CUDA
