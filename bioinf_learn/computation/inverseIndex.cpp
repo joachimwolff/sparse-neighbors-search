6a2abe2b006f9634dd09b15d0d76ed48206ea309
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-12-07 18:55:25 +0100

    Added SSE version skeleton function for minHash computation

--- bioinf_learn/computation/inverseIndex.cpp
+++ bioinf_learn/computation/inverseIndex.cpp
@@ -19,6 +19,7 @@
 #ifdef OPENMP
 #include <omp.h>
 #endif
+#include <nmmintrin.h>
 
 #include <time.h>
 #include "inverseIndex.h"
@@ -97,38 +98,38 @@ distributionInverseIndex* InverseIndex::getDistribution() {
 }
 
 // compute the signature for one instance with SSE support
-// vsize_t* InverseIndex::computeSignatureSSE(SparseMatrixFloat* pRawData, const size_t pInstance) {
+vsize_t* InverseIndex::computeSignatureSSE(SparseMatrixFloat* pRawData, const size_t pInstance) {
 
-//     if (pRawData == NULL) return NULL;
-//     vsize_t* signature = new vsize_t(mNumberOfHashFunctions * mBlockSize);
-//     size_t argmin = 0;
-//     for(size_t j = 0; j < mNumberOfHashFunctions * mBlockSize; ++j) {
-//             size_t nearestNeighborsValue = MAX_VALUE;      
-//             size_t iterations = pRawData->getSizeOfInstance(pInstance);
-//             __m128i minimumVector = {MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE};
-//             for (size_t i = 0; i < iterations; i+=4) {
-//                 __m128i value = {(pRawData->getNextElement(pInstance, i) +1), 
-//                                     (pRawData->getNextElement(pInstance, i+1) +1),
-//                                     (pRawData->getNextElement(pInstance, i+2) +1),
-//                                     (pRawData->getNextElement(pInstance, i+3) +1)};
-//                 __m128i hashValue = mHash->hashSSE(value, (j+1), MAX_VALUE);
+    if (pRawData == NULL) return NULL;
+    vsize_t* signature = new vsize_t(mNumberOfHashFunctions * mBlockSize);
+    size_t argmin = 0;
+    for(size_t j = 0; j < mNumberOfHashFunctions * mBlockSize; ++j) {
+            size_t nearestNeighborsValue = MAX_VALUE;      
+            size_t iterations = pRawData->getSizeOfInstance(pInstance);
+            __m128i minimumVector = {MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE};
+            for (size_t i = 0; i < iterations; i+=4) {
+                __m128i value = {(pRawData->getNextElement(pInstance, i) +1), 
+                                    (pRawData->getNextElement(pInstance, i+1) +1),
+                                    (pRawData->getNextElement(pInstance, i+2) +1),
+                                    (pRawData->getNextElement(pInstance, i+3) +1)};
+                __m128i hashValue = mHash->hashSSE(value, (j+1), MAX_VALUE);
 
-                
-//                 if (hashValue < nearestNeighborsValue) {
-//                     nearestNeighborsValue = hashValue;
-//                     argmin = pRawData->getNextElement(pInstance, i);
-//                 }
-//             }
+                // compare 
+                if (hashValue < nearestNeighborsValue) {
+                    nearestNeighborsValue = hashValue;
+                    argmin = pRawData->getNextElement(pInstance, i);
+                }
+            }
 
-//             (*signature)[j] = argmin;
+            (*signature)[j] = argmin;
             
-//     }
-//     // reduce number of hash values by a factor of mShingleSize
-//     if (mShingle) {
-//         return shingle(signature);
-//     }
-//     return signature;
-// }  
+    }
+    // reduce number of hash values by a factor of mShingleSize
+    if (mShingle) {
+        return shingle(signature);
+    }
+    return signature;
+}  
 // compute the signature for one instance
 vsize_t* InverseIndex::computeSignature(SparseMatrixFloat* pRawData, const size_t pInstance) {
 
