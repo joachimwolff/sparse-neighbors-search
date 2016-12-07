5c57ace69acf5e3b002d39c6a72d1f1d78506409
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-12-07 18:54:47 +0100

    Extended hash sse version

--- bioinf_learn/computation/hash.h
+++ bioinf_learn/computation/hash.h
@@ -1,15 +1,60 @@
 #include "typeDefinitions.h"
 #include <functional>
 #include <xmmintrin.h>
+#include <emmintrin.h>
+#include <intrin.h>
 #ifndef HASH_H
 #define HASH_H
 class Hash {
   private:
     const double A = sqrt(2) - 1;
-    
-    // hash_SSE() {
+    _mm_not_si128 (__m128i x) {
+	    // Returns ~x, the bitwise complement of x:
+	    return _mm_xor_si128(x, _mm_cmpeq_epi32(_mm_setzero_si128(), _mm_setzero_si128()));
+    }
+    __m128i hash_SSE(__m128i keys, _m128i aModulo) {
+        // key = key * A;
+        __m128i keys_temp = {A, A, A, A};
+        keys = _mm_mul_epi32(keys_temp, keys);
+        
+        // key = ~key + (key << 15); 
+        keys_temp = keys;
+        // negate keys_temp
+        keys_temp = _mm_not_si128(keys_temp);
+        // left shift 15 bits
+        keys = _mm_slli_epi32(keys, 15);
+        keys = _mm_add_epi32(keys_temp, keys);
 
-    // }
+        // key = key ^ (key >> 12);
+        keys_temp = keys;
+        keys = _mm_srli_epi32(keys, 12);
+        keys = _mm_xor_si128(keys_tmp, keys);
+
+        // key = key + (key << 2);
+        keys_temp = keys;
+        keys = _mm_slli_epi32(keys, 2);
+        keys = _mm_add_epi32(keys_temp, keys);
+
+        // key = key ^ (key >> 4);
+        keys_temp = keys;
+        keys = _mm_srli_epi32(keys, 4);
+        keys = _mm_xor_si128(keys_tmp, keys);           
+
+        // key = key * 2057;
+        keys_temp = {2057, 2057, 2057, 2057};
+        keys = _mm_mul_epi32(keys_temp, keys);
+
+        // key = key ^ (key >> 16);
+        keys_temp = keys;
+        keys = _mm_srli_epi32(keys, 16);
+        keys = _mm_xor_si128(keys_tmp, keys); 
+        // return key % aModulo;
+        
+        
+
+        
+
+    }
     size_t size_tHashSimple(size_t key, size_t aModulo) {
           // source:  Thomas Wang: Integer Hash Functions, 1997 / 2007 
           // https://gist.github.com/badboy/6267743
