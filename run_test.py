263802d8525d0775189da3b26473f8cf64109f07
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-12-21 14:14:04 +0100

    extended file

--- run_test.py
+++ run_test.py
@@ -5,4 +5,6 @@ from sklearn_freiburg_extension import MinHashClassifier
 from sklearn_freiburg_extension import WtaHash
 from sklearn_freiburg_extension import WtaHashClassifier
 
-n_neighbors_minHash = MinHash(n_neighbors = 4)
+
+if __name__ == "__main__":
+    n_neighbors_minHash = MinHash(n_neighbors = 4)
