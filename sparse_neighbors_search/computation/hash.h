fd43d00847e4b6c40bde2697a14d0eaa244e580e
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-01-22 19:25:45 +0100

    sse support for the hashing algorithm

--- sparse_neighbors_search/computation/hash.h
+++ sparse_neighbors_search/computation/hash.h
@@ -2,67 +2,77 @@
 #include <functional>
 #include <xmmintrin.h>
 #include <emmintrin.h>
-#include <intrin.h>
+// #include <intrin.h>
 #ifndef HASH_H
 #define HASH_H
 class Hash {
   private:
     const double A = sqrt(2) - 1;
-    _mm_not_si128 (__m128i x) {
+    const float A_float = sqrt(2) - 1;
+    
+    __m128i _mm_not_si128 (__m128i x) {
 	    // Returns ~x, the bitwise complement of x:
 	    return _mm_xor_si128(x, _mm_cmpeq_epi32(_mm_setzero_si128(), _mm_setzero_si128()));
     }
-    __m128i hash_SSE_priv(__m128i keys, _m128i aModulo) {
-        // key = key * A;
-        __m128i keys_temp = {A, A, A, A};
-        keys = _mm_mul_epi32(keys_temp, keys);
+    __m128i hash_SSE_priv(__m128i keys) {
+
+        __m128 keys_temp = {A_float, A_float, A_float, A_float};
+        // conversion to float
+        __m128 keys_float = _mm_cvtepi32_ps(keys);
+        // multiplication of key * A
+        keys_float =_mm_mul_ps(keys_float, keys_temp);
+        // conversion back to int
+        keys = _mm_cvtps_epi32(keys_float);
+
         
-        // key = ~key + (key << 15); 
-        keys_temp = keys;
-        // negate keys_temp
-        keys_temp = _mm_not_si128(keys_temp);
-        // left shift 15 bits
-        keys = _mm_slli_epi32(keys, 15);
-        keys = _mm_add_epi32(keys_temp, keys);
+        // // key = ~key + (key << 15); 
+        // __m128i keys_temp_int = keys;
+        // // negate keys_temp
+        // keys_temp_int = _mm_not_si128(keys_temp_int);
+        // // left shift 15 bits
+        // keys = _mm_slli_epi32(keys, 15);
+        // keys = _mm_add_epi32(keys_temp_int, keys);
 
-        // key = key ^ (key >> 12);
-        keys_temp = keys;
-        keys = _mm_srli_epi32(keys, 12);
-        keys = _mm_xor_si128(keys_tmp, keys);
+        // // key = key ^ (key >> 12);
+        // keys_temp_int = keys;
+        // keys = _mm_srli_epi32(keys, 12);
+        // keys = _mm_xor_si128(keys_temp_int, keys);
 
-        // key = key + (key << 2);
-        keys_temp = keys;
-        keys = _mm_slli_epi32(keys, 2);
-        keys = _mm_add_epi32(keys_temp, keys);
+        // // key = key + (key << 2);
+        // keys_temp_int = keys;
+        // keys = _mm_slli_epi32(keys, 2);
+        // keys = _mm_add_epi32(keys_temp_int, keys);
 
-        // key = key ^ (key >> 4);
-        keys_temp = keys;
-        keys = _mm_srli_epi32(keys, 4);
-        keys = _mm_xor_si128(keys_tmp, keys);           
+        // // key = key ^ (key >> 4);
+        // keys_temp_int = keys;
+        // keys = _mm_srli_epi32(keys, 4);
+        // keys = _mm_xor_si128(keys_temp_int, keys);           
 
-        // key = key * 2057;
-        keys_temp = {2057, 2057, 2057, 2057};
-        keys = _mm_mul_epi32(keys_temp, keys);
+        // // key = key * 2057;
+        // uint32_t value = 2057;
+        // __m128i constant_value = _mm_set_epi32(value, value, value, value);
+        // keys = _mm_mul_epi32(constant_value, keys);
 
-        // key = key ^ (key >> 16);
-        keys_temp = keys;
-        keys = _mm_srli_epi32(keys, 16);
-        keys = _mm_xor_si128(keys_tmp, keys); 
+        // // key = key ^ (key >> 16);
+        // keys_temp_int = keys;
+        // keys = _mm_srli_epi32(keys, 16);
+        // keys = _mm_xor_si128(keys_temp_int, keys); 
+
         // return key % aModulo;
-        
+        // exit(0);
         return keys;
 
     }
-    size_t size_tHashSimple(size_t key, size_t aModulo) {
+    uint32_t size_tHashSimple(uint32_t key, uint32_t aModulo) {
           // source:  Thomas Wang: Integer Hash Functions, 1997 / 2007 
           // https://gist.github.com/badboy/6267743
           key = key * A;
-          key = ~key + (key << 15);
-          key = key ^ (key >> 12);
-          key = key + (key << 2);
-          key = key ^ (key >> 4);
-          key = key * 2057;
-          key = key ^ (key >> 16);
+        //   key = ~key + (key << 15);
+        //   key = key ^ (key >> 12);
+        //   key = key + (key << 2);
+        //   key = key ^ (key >> 4);
+        //   key = key * 2057;
+        //   key = key ^ (key >> 16);
           return key % aModulo;
     }; 
     
@@ -77,8 +87,9 @@ class Hash {
           return key % aModulo;
     };
   public:      
-    size_t hash(size_t pKey, size_t pSeed, size_t pModulo) {
-        return size_tHashSimple(pKey * pSeed, pModulo);
+    uint32_t hash(uint32_t pKey, uint32_t pSeed, uint32_t pModulo) {
+        return pKey * pSeed;
+        // return size_tHashSimple(pKey * pSeed, pModulo);
     };
     short unsigned int hashShort(short unsigned int pKey, short unsigned int pSeed, short unsigned int pModulo) {
         return shortHashSimple(pKey * pSeed, pModulo);
@@ -89,9 +100,10 @@ class Hash {
         
         return hash_function(pKey*pSeed) % pModulo;
     };
-    __m128i hash_SSE(__m128i pKeys, __m128i pSeed, __m128i pModulo) {
+    __m128i hash_SSE(__m128i pKeys, __m128i pSeed) {
         pKeys = _mm_mul_epi32(pKeys, pSeed);
-        return hash_SSE_priv(pKeys, pModulo);
+        return pKeys;
+        // return hash_SSE_priv(pKeys);
     }
 };
 #endif // HASH_H
