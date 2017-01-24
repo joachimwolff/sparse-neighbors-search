78cf8ae9335bc1837b880cb83e5ff63ac4135353
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-01-24 21:54:06 +0100

    Playing around

--- sparse_neighbors_search/computation/debugMain.cpp
+++ sparse_neighbors_search/computation/debugMain.cpp
@@ -8,11 +8,11 @@ int main(int** args) {
     Hash* hash = new Hash();
     int a[4] = {1, 67, 800, 1212123};
 
-    std::vector<uint32_t> foo = {1, 67, 800, 1212123};
+    std::vector<uint32_t> foo = {1, 67, 800, 1337};
     // __m128i foo_SSE = _mm_setzero_si128();
-    __m128i foo_SSE = _mm_setr_epi32(1, 67, 800, 1212123);
+    __m128i foo_SSE = _mm_setr_epi32(1, 67, 800, 1337);
      
-    __m128i seed_SSE = _mm_set_epi32(2, 2, 2, 2);
+    __m128i seed_SSE = _mm_setr_epi32(2, 2, 2, 2);
     __m128i foo_result_SSE = hash->hash_SSE(foo_SSE, seed_SSE);
     // std::cout << "Size: " << foo_SSE->size() << std::endl;
     for(size_t i = 0; i < 4; ++i) {
@@ -21,10 +21,10 @@ int main(int** args) {
         std::cout << "Traditional: " << hash->hash(foo[i], 2, MAX_VALUE) << std::endl;
         // std::cout << "SSE: " << foo_result_SSE[i] % MAX_VALUE << std::endl;
     }
-        std::cout << "\n\nNumberSSE " << 0 << ": \t" << _mm_extract_epi32(foo_result_SSE, 0) << std::endl;
-        std::cout << "NumberSSE " << 1 << ": \t" << _mm_extract_epi32(foo_result_SSE, 1) << std::endl;
-        std::cout << "NumberSSE " << 2 << ": \t" << _mm_extract_epi32(foo_result_SSE, 2) << std::endl;
-        std::cout << "NumberSSE " << 3 << ": \t" << _mm_extract_epi32(foo_result_SSE, 3) << std::endl;
+        std::cout << "\n\nNumberSSE " << 0 << ": \t" << (uint32_t) _mm_extract_epi32(foo_result_SSE, 0) << std::endl;
+        std::cout << "NumberSSE " << 1 << ": \t" << (uint32_t) _mm_extract_epi32(foo_result_SSE, 1) << std::endl;
+        std::cout << "NumberSSE " << 2 << ": \t" <<(uint32_t) _mm_extract_epi32(foo_result_SSE, 2) << std::endl;
+        std::cout << "NumberSSE " << 3 << ": \t" << (uint32_t) _mm_extract_epi32(foo_result_SSE, 3) << std::endl;
   
     delete hash;
 }
