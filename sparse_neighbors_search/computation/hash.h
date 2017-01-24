af88aca4017ca2d59311c8bd363cd9ed6af2e471
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-01-24 19:50:53 +0100

    hash function works with sse now

--- sparse_neighbors_search/computation/hash.h
+++ sparse_neighbors_search/computation/hash.h
@@ -3,17 +3,15 @@
 #include <xmmintrin.h>
 #include <emmintrin.h>
 // #include <intrin.h>
+#include "sseExtension.h"
 #ifndef HASH_H
 #define HASH_H
 class Hash {
   private:
     const double A = sqrt(2) - 1;
     const float A_float = sqrt(2) - 1;
+
     
-    __m128i _mm_not_si128 (__m128i x) {
-	    // Returns ~x, the bitwise complement of x:
-	    return _mm_xor_si128(x, _mm_cmpeq_epi32(_mm_setzero_si128(), _mm_setzero_si128()));
-    }
     __m128i hash_SSE_priv(__m128i keys) {
 
         __m128 keys_temp = {A_float, A_float, A_float, A_float};
@@ -22,8 +20,7 @@ class Hash {
         // multiplication of key * A
         keys_float = _mm_mul_ps(keys_float, keys_temp);
         // conversion back to int
-        keys = _mm_cvtps_epi32(keys_float);
-
+        keys = _mm_cvttps_epi32(keys_float);
         
         // key = ~key + (key << 15); 
         __m128i keys_temp_int = keys;
@@ -33,14 +30,17 @@ class Hash {
         keys = _mm_slli_epi32(keys, 15);
         keys = _mm_add_epi32(keys_temp_int, keys);
 
+
+
         // key = key ^ (key >> 12);
         keys_temp_int = keys;
         keys = _mm_srli_epi32(keys, 12);
         keys = _mm_xor_si128(keys_temp_int, keys);
-
+        
         // key = key + (key << 2);
         keys_temp_int = keys;
         keys = _mm_slli_epi32(keys, 2);
+
         keys = _mm_add_epi32(keys_temp_int, keys);
 
         // key = key ^ (key >> 4);
@@ -51,7 +51,8 @@ class Hash {
         // key = key * 2057;
         uint32_t value = 2057;
         __m128i constant_value = _mm_set_epi32(value, value, value, value);
-        keys = _mm_mul_epi32(constant_value, keys);
+        keys = _mm_mul_epi32_4int(constant_value, keys);
+        // return keys;
 
         // key = key ^ (key >> 16);
         keys_temp_int = keys;
@@ -67,12 +68,19 @@ class Hash {
           // source:  Thomas Wang: Integer Hash Functions, 1997 / 2007 
           // https://gist.github.com/badboy/6267743
           key = key * A;
-        //   key = ~key + (key << 15);
-        //   key = key ^ (key >> 12);
-        //   key = key + (key << 2);
-        //   key = key ^ (key >> 4);
-        //   key = key * 2057;
-        //   key = key ^ (key >> 16);
+          
+          key = ~key + (key << 15);
+          key = key ^ (key >> 12);
+          
+          key = key + (key << 2);
+            // key = (key << 2);
+          
+          key = key ^ (key >> 4);
+        
+          key = key * 2057;
+        //   return key;
+          
+          key = key ^ (key >> 16);
           return key % aModulo;
     }; 
     
@@ -101,7 +109,8 @@ class Hash {
         return hash_function(pKey*pSeed) % pModulo;
     };
     __m128i hash_SSE(__m128i pKeys, __m128i pSeed) {
-        pKeys = _mm_mul_epu32(pKeys, pSeed);
+        pKeys = _mm_mul_epi32_4int(pKeys, pSeed);
+
         // return pKeys;
         return hash_SSE_priv(pKeys);
     }
