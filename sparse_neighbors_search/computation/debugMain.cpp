6b0238f15bc9c95b7a2ca660cb1bc4a5272c5e7d
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-01-24 13:26:25 +0100

    playing around

--- sparse_neighbors_search/computation/debugMain.cpp
+++ sparse_neighbors_search/computation/debugMain.cpp
@@ -10,18 +10,21 @@ int main(int** args) {
 
     std::vector<uint32_t> foo = {1, 67, 800, 1212123};
     // __m128i foo_SSE = _mm_setzero_si128();
-    __m128i foo_SSE = _mm_load_epi32(a);
+    __m128i foo_SSE = _mm_setr_epi32(1, 67, 800, 1212123);
      
-    __m128i seed_SSE = _mm_set_epi32(1, 1, 1, 1);
+    __m128i seed_SSE = _mm_set_epi32(2, 2, 2, 2);
     __m128i foo_result_SSE = hash->hash_SSE(foo_SSE, seed_SSE);
-    
+    // std::cout << "Size: " << foo_SSE->size() << std::endl;
     for(size_t i = 0; i < 4; ++i) {
-        std::cout << "\n\nNumber " << i << ": \t" << foo[i] << std::endl;
-        std::cout << "NumberSSE " << i << ": \t" << foo_SSE[i] << std::endl;
+        // std::cout << "\n\nNumber " << i << ": \t" << foo[i] << std::endl;
         
-        // std::cout << "\n\nTraditional: " << hash->hash(foo[i], 1, MAX_VALUE) << std::endl;
+        std::cout << "Traditional: " << hash->hash(foo[i], 2, MAX_VALUE) << std::endl;
         // std::cout << "SSE: " << foo_result_SSE[i] % MAX_VALUE << std::endl;
     }
+        std::cout << "\n\nNumberSSE " << 0 << ": \t" << _mm_extract_epi32(foo_result_SSE, 0) << std::endl;
+        std::cout << "NumberSSE " << 1 << ": \t" << _mm_extract_epi32(foo_result_SSE, 1) << std::endl;
+        std::cout << "NumberSSE " << 2 << ": \t" << _mm_extract_epi32(foo_result_SSE, 2) << std::endl;
+        std::cout << "NumberSSE " << 3 << ": \t" << _mm_extract_epi32(foo_result_SSE, 3) << std::endl;
   
     delete hash;
 }
